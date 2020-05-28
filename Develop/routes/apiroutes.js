853c62fdd9776c5b4e66125b06f12d02578a451c
Christina Ausley <christinamausley@gmail.com>
2020-05-27 19:51:41 -0700

    updated apiroutes

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,14 +1,39 @@
 // Connect routes to data.
 
-
+const fs = require('fs');
+const path = require('path');
 // Routing
 module.exports = function (app) {
 
   // GET request
-  app.get
+  app.get('/api/notes', function(req, res) {
+    
+    var notes = fs.readFileSync(path.join(__dirname, '../db/db.json'));
+    notes = JSON.parse(notes);
+    res.json(notes);
+  });
 
   // POST request
-  app.post
+  app.post('/api/notes', function(req, res) {
+    var newNote = {
+      title: req.body.title,
+      text: req.body.text,
+    };
+
+    
+
+    var notes = fs.readFileSync(path.join(__dirname, '../db/db.json'));
+    newNote.id = String(notes.length);
+    notes = JSON.parse(notes);
+    notes.push(newNote);
+
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(notes));
+
+    res.json(notes);
+  });
 
   // DELETE request
-  app.delete
+  app.delete('/api/notes/:noteId', function(req, res) {
+    const noteId = req.params.noteId;
+  });
+};
