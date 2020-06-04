b7c55c4b9c5432b6746db451c0112cbb6009b19b
Christina Ausley <christinamausley@gmail.com>
2020-06-04 09:35:13 -0700

    updated filepath

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.use(express.static("Develop/public"));
+app.use(express.static("/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-require("./Develop/routes/apiRoutes")(app);
-require("./Develop/routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
